f21266865756789504f98e5b007f0b17108c145b
IncendioHawk <111070562+IncendioHawk@users.noreply.github.com>
2024-02-19 21:16:26 +0000

    Did stuff

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <main>
+      <form action="https://ubiquitous-broccoli-wpxvrvgrjg62569x-3000.app.github.dev/login" method="post" className="login-form">
+        <label htmlFor="username">Username:</label>
+        <input required type="text" id="username" name="username" />
+        <label htmlFor="password">Password:</label>
+        <input required type="password" id="password" name="password" />
+        <button type="submit">Login</button>
+      </form>
+    </main>
   )
 }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 const app = express()
 require("dotenv").config()
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const port = process.env.PORT
 
+app.post('/login', (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+  res.send(`Username: ${username}, Password: ${password}`)
+})
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`)
